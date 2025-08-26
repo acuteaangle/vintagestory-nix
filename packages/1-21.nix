21063d31c21969d5563f2d01b75120a1d8bb1b1b
PierreBorine <pierre.borine@protonmail.com>
2025-08-26 19:22:19 +0200

    vintagestory: 1.21.0

---
+++ packages/1-21.nix
@@ -0,0 +1,6 @@
+mkVSVersion: [
+  (mkVSVersion {
+    version = "1.21.0";
+    hash = "1c6dibxx3q7b3rfn9sigb1h2ijdpzxrzxrih6r3k8wl9xfn7mnh1";
+  })
+]
--- packages/default.nix
+++ packages/default.nix
@@ -10,7 +10,8 @@
   inherit (clib) recursiveMergeAttrsList mkLatest;
 in
   recursiveMergeAttrsList [
-    (mkLatest "1.20") # Must be changed manually
+    (mkLatest "1.21") # WARN: Must be changed manually
+    (v1.mkMinorVersion (import ./1-21.nix v1.mkVSVersion))
     (v1.mkMinorVersion (import ./1-20.nix v1.mkVSVersion))
     (v1.mkMinorVersion (import ./1-19.nix v1.mkVSVersion))
     (v1.mkMinorVersion (import ./1-18.nix v1.mkVSVersion))
