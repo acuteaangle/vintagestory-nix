1ded2535ee627daf4278a5382ab01362f8bb7762
PierreBorine <pierre.borine@protonmail.com>
2025-09-08 14:00:53 +0200

    refactor: import some functions on top for better readability

--- packages/libs/default.nix
+++ packages/libs/default.nix
@@ -2,32 +2,46 @@
   packages,
   lib,
 }: let
+  inherit
+    (builtins)
+    foldl'
+    replaceStrings
+    head
+    attrValues
+    filter
+    match
+    compareVersions
+    warn
+    ;
+  inherit (lib.attrsets) recursiveUpdate;
+  inherit (lib.versions) majorMinor;
+
   recursiveMergeAttrsList =
-    builtins.foldl' (acc: attr: lib.attrsets.recursiveUpdate acc attr) {};
+    foldl' (acc: attr: recursiveUpdate acc attr) {};
 
   # "1.20.4" => "1-20-4"
-  normalizeVersion = builtins.replaceStrings ["."] ["-"];
+  normalizeVersion = replaceStrings ["."] ["-"];
 
-  getVersion = set: (builtins.head (builtins.attrValues set)).version;
+  getVersion = set: (head (attrValues set)).version;
   highestVersion = versions': let
-    versions = builtins.filter (el: builtins.match ".*-rc\.[0-9]{1,2}" (getVersion el) == null) versions';
+    versions = filter (el: match ".*-rc\.[0-9]{1,2}" (getVersion el) == null) versions';
   in
-    builtins.foldl' (
+    foldl' (
       maxSet: set: let
         maxSetVersion = getVersion maxSet;
         setVersion = getVersion set;
       in
-        if builtins.compareVersions maxSetVersion setVersion == 1
+        if compareVersions maxSetVersion setVersion == 1
         then maxSet // {version = maxSetVersion;}
         else set // {version = setVersion;}
-    ) (builtins.head versions)
+    ) (head versions)
     versions;
 
   mkLatest = version: let
-    v = normalizeVersion (lib.versions.majorMinor version);
+    v = normalizeVersion (majorMinor version);
   in rec {
     latest = packages."v${v}";
-    latest-net8 = builtins.warn ''
+    latest-net8 = warn ''
       'latest-net8' is deprecated, please use 'latest' instead. As of 1.21, Vintage Story officially uses dotnet8.
     '' latest;
   };
