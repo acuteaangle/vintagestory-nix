467b81d568f128d917b6833a3f3429fe32142872
PierreBorine <pierre.borine@protonmail.com>
2025-09-08 13:31:32 +0200

    fix: filter unstable releases out from major.minor and latest packages

--- packages/libs/default.nix
+++ packages/libs/default.nix
@@ -9,7 +9,9 @@
   normalizeVersion = builtins.replaceStrings ["."] ["-"];
 
   getVersion = set: (builtins.head (builtins.attrValues set)).version;
-  highestVersion = versions:
+  highestVersion = versions': let
+    versions = builtins.filter (el: builtins.match ".*-rc\.[0-9]{1,2}" (getVersion el) == null) versions';
+  in
     builtins.foldl' (
       maxSet: set: let
         maxSetVersion = getVersion maxSet;
@@ -26,8 +28,7 @@
   in rec {
     latest = packages."v${v}";
     latest-net8 = builtins.warn ''
-      'latest-net8' is deprecated, please use 'latest' instead.
-      As of 1.21, Vintage Story officially uses dotnet8.
+      'latest-net8' is deprecated, please use 'latest' instead. As of 1.21, Vintage Story officially uses dotnet8.
     '' latest;
   };
 in {
