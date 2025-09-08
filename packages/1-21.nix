fd12f34bee04a84936018f5e8d4ce2f7265bf374
PierreBorine <pierre.borine@protonmail.com>
2025-09-08 12:43:26 +0200

    vintagestory: 1.21.1-rc.1, 1.21.1-rc.2

--- packages/1-21.nix
+++ packages/1-21.nix
@@ -3,4 +3,14 @@ mkVSVersion: [
     version = "1.21.0";
     hash = "1c6dibxx3q7b3rfn9sigb1h2ijdpzxrzxrih6r3k8wl9xfn7mnh1";
   })
+  (mkVSVersion {
+    version = "1.21.1-rc.1";
+    hash = "sha256-jTImsd8KQNTAL8MFUl7pp3H/URyPGzvh1VKRTQPVc0c=";
+    unstable = true;
+  })
+  (mkVSVersion {
+    version = "1.21.1-rc.2";
+    hash = "sha256-mSYcwWTlTmiM1/tI2iecqZIwPIdR3Rxqda4rwE1COho=";
+    unstable = true;
+  })
 ]
