a459c9eeec10cff7102cbc506580d3268f11642f
AcuteAangle <acuteaangle@disroot.org>
2025-09-09 08:58:50 -0400

    Add 1.21.1

--- packages/1-21.nix
+++ packages/1-21.nix
@@ -3,4 +3,8 @@ mkVSVersion: [
     version = "1.21.0";
     hash = "sha256-Adp6rOuJcjRHNjDm/nP/t8koYFgv6mRdHuvg0fuKzbA=";
   })
+  (mkVSVersion {
+    version = "1.21.1";
+    hash = "sha256-M5IRghH980f7taxwvXbvKT+ggNIz47h93VFhm49ZAE0=";
+  })
 ]
