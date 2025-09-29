1629d697dbe2eaef88b0da93e6e703b00dffbea1
PierreBorine <pierre.borine@protonmail.com>
2025-09-30 01:50:39 +0200

    rustique: 0.5.10 -> 0.5.11

--- tools/rustique/default.nix
+++ tools/rustique/default.nix
@@ -5,13 +5,13 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "Rustique";
-  version = "0.5.10";
+  version = "0.5.11";
 
   src = fetchFromGitHub {
     owner = "Tekunogosu";
     repo = "Rustique";
     rev = "v${version}";
-    hash = "sha256-1hjhJD5kCaFtwDbOvuvRLNhN8OnmMh3rysj24+jmKCU=";
+    hash = "sha256-CdVZbp3csZ0uQImYBeEhTGLAwL5EA42sefmjylBStMY=";
   };
 
   # tries to use clang and /usr/bin/mold, let's just not do that, and
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage rec {
   postPatch = "rm -vf .cargo/config.toml";
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-CMaQI3CkO3byXgzsRgdFzm3a3VW3AR8Z5FA2+zIUFmI=";
+  cargoHash = "sha256-pIaMiPe3OW6JU5NKUhsVHNsvYvQdIVs1swhWz0wPn08=";
 
   # unstable rust feature path_add_extension
   env.RUSTC_BOOTSTRAP = 1;
