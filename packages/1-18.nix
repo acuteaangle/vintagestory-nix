04ea3d50848a10dd472b14ff186fe254cbac3217
PierreBorine <pierre.borine@protonmail.com>
2025-09-30 03:23:06 +0200

    style(vintagestory): sort version in ascending order

--- packages/1-18.nix
+++ packages/1-18.nix
@@ -1,35 +1,35 @@
 mkVSVersion: [
   (mkVSVersion {
-    version = "1.18.15";
-    hash = "0syy29683hvf3wvz44gaq2lib3q07zmhdxkwxhdpxvw7cjnaf1l2";
-  })
-  (mkVSVersion {
-    version = "1.18.14";
-    hash = "04kr1jfdw95jf4j9cgk699z951yqwdx6nz2pvci7734cw7n37shp";
-  })
-  (mkVSVersion {
-    version = "1.18.13";
-    hash = "1drrv3d5fg90a9i5zzzib0bagk79ps5l5rbjqxwbz9i3898iacj1";
-  })
-  (mkVSVersion {
-    version = "1.18.12";
-    hash = "0lrvzshqmx916xh32c6y30idqpmfi6my6w26l3h32y7lkx26whc6";
-  })
-  (mkVSVersion {
-    version = "1.18.11";
-    hash = "sha256-hEz5dC4ftFS0QYpcuChxkXYX2Bjr6mwJ3qcnBQNmaFY=";
-  })
-  (mkVSVersion {
-    version = "1.18.10";
-    hash = "sha256-HR5eoNkY6BVYNtIDyDKSoWwSde9MpObw+IdKXGZhj70=";
+    version = "1.18.8";
+    hash = "sha256-cBP+nqaIq7dIcKyyIuw/w0q5/lhcClOZ9N/O1Kuu2qk=";
   })
   (mkVSVersion {
     version = "1.18.9";
     hash = "sha256-w+BD7vi8xsGO9db2wzgHdBiSZsW8CpimC/8TugNOjhE=";
   })
   (mkVSVersion {
-    version = "1.18.8";
-    hash = "sha256-cBP+nqaIq7dIcKyyIuw/w0q5/lhcClOZ9N/O1Kuu2qk=";
+    version = "1.18.10";
+    hash = "sha256-HR5eoNkY6BVYNtIDyDKSoWwSde9MpObw+IdKXGZhj70=";
+  })
+  (mkVSVersion {
+    version = "1.18.11";
+    hash = "sha256-hEz5dC4ftFS0QYpcuChxkXYX2Bjr6mwJ3qcnBQNmaFY=";
+  })
+  (mkVSVersion {
+    version = "1.18.12";
+    hash = "0lrvzshqmx916xh32c6y30idqpmfi6my6w26l3h32y7lkx26whc6";
+  })
+  (mkVSVersion {
+    version = "1.18.13";
+    hash = "1drrv3d5fg90a9i5zzzib0bagk79ps5l5rbjqxwbz9i3898iacj1";
+  })
+  (mkVSVersion {
+    version = "1.18.14";
+    hash = "04kr1jfdw95jf4j9cgk699z951yqwdx6nz2pvci7734cw7n37shp";
+  })
+  (mkVSVersion {
+    version = "1.18.15";
+    hash = "0syy29683hvf3wvz44gaq2lib3q07zmhdxkwxhdpxvw7cjnaf1l2";
   })
   # Versions past this used to have a different derivation in nixpkgs.
   # Hopefully, no one needs such old versions, I don't see why bother packaging them.
--- packages/1-20.nix
+++ packages/1-20.nix
@@ -1,54 +1,54 @@
 mkVSVersion: [
   (mkVSVersion {
-    version = "1.20.12";
-    hash = "0pwxpry72bv9h0vi0fb8yngz0fiq6ypkrvhzgscmr4q1fjk42p0s";
-  })
-  (mkVSVersion {
-    version = "1.20.11";
-    hash = "1fcgbzm73qmgf5w27ym6555yy3dnlblw0rwrfj4h5vb6y3k9l92m";
-  })
-  (mkVSVersion {
-    version = "1.20.10";
-    hash = "115pgm36lb06hbqljclhdaskd8w93pnir0dan9c6yv64m1kxip08";
-  })
-  (mkVSVersion {
-    version = "1.20.9";
-    hash = "1wbz9if2wkhjir3d4w4jld4ric0y8dgqgp53mhr2jyw102p9kn4z";
-  })
-  (mkVSVersion {
-    version = "1.20.8";
-    hash = "0j528b72z1dnb2dp6jpnpvfhvzv6v8bnhffvm0phxbbskskyh1z0";
-  })
-  (mkVSVersion {
-    version = "1.20.7";
-    hash = "1d1cvxzz501gb5kd1lh9dvkl75p9kvb2bcx75glbzy11pp5zwxxn";
-  })
-  (mkVSVersion {
-    version = "1.20.6";
-    hash = "0j8j88csy0brbbxzqkxrdp9mxv3nvagg3p4222m1zsrgwl3jki5j";
-  })
-  (mkVSVersion {
-    version = "1.20.5";
-    hash = "0pggrl8yix77zcg5hkg9xiczcr87ql3yq1bvwnyqqr2kcjnjkjvg";
-  })
-  (mkVSVersion {
-    version = "1.20.4";
-    hash = "1p43h5cycm7fqpp06p8rkxhgivr79lalcz8r2fc45vdgxp416ns4";
-  })
-  (mkVSVersion {
-    version = "1.20.3";
-    hash = "04hqvzfn4vpbbcw47x3fbznzi4rdsx6szcpxhppjpzfznhab5qkf";
-  })
-  (mkVSVersion {
-    version = "1.20.2";
-    hash = "02gzzgmmfidzdsl0lr13q3n1i2w6j0i2nz8m52zm8379q7l6ficx";
+    version = "1.20.0";
+    hash = "05wy6vs6sm6gcq94r5znaw4xrj4h9cfn7cwivxm12wij3fj0z9vf";
   })
   (mkVSVersion {
     version = "1.20.1";
     hash = "0gb4ypxjp15cfa3dzyp75gb04jwnvngim16iywnzir8iaicrjjn9";
   })
   (mkVSVersion {
-    version = "1.20.0";
-    hash = "05wy6vs6sm6gcq94r5znaw4xrj4h9cfn7cwivxm12wij3fj0z9vf";
+    version = "1.20.2";
+    hash = "02gzzgmmfidzdsl0lr13q3n1i2w6j0i2nz8m52zm8379q7l6ficx";
+  })
+  (mkVSVersion {
+    version = "1.20.3";
+    hash = "04hqvzfn4vpbbcw47x3fbznzi4rdsx6szcpxhppjpzfznhab5qkf";
+  })
+  (mkVSVersion {
+    version = "1.20.4";
+    hash = "1p43h5cycm7fqpp06p8rkxhgivr79lalcz8r2fc45vdgxp416ns4";
+  })
+  (mkVSVersion {
+    version = "1.20.5";
+    hash = "0pggrl8yix77zcg5hkg9xiczcr87ql3yq1bvwnyqqr2kcjnjkjvg";
+  })
+  (mkVSVersion {
+    version = "1.20.6";
+    hash = "0j8j88csy0brbbxzqkxrdp9mxv3nvagg3p4222m1zsrgwl3jki5j";
+  })
+  (mkVSVersion {
+    version = "1.20.7";
+    hash = "1d1cvxzz501gb5kd1lh9dvkl75p9kvb2bcx75glbzy11pp5zwxxn";
+  })
+  (mkVSVersion {
+    version = "1.20.8";
+    hash = "0j528b72z1dnb2dp6jpnpvfhvzv6v8bnhffvm0phxbbskskyh1z0";
+  })
+  (mkVSVersion {
+    version = "1.20.9";
+    hash = "1wbz9if2wkhjir3d4w4jld4ric0y8dgqgp53mhr2jyw102p9kn4z";
+  })
+  (mkVSVersion {
+    version = "1.20.10";
+    hash = "115pgm36lb06hbqljclhdaskd8w93pnir0dan9c6yv64m1kxip08";
+  })
+  (mkVSVersion {
+    version = "1.20.11";
+    hash = "1fcgbzm73qmgf5w27ym6555yy3dnlblw0rwrfj4h5vb6y3k9l92m";
+  })
+  (mkVSVersion {
+    version = "1.20.12";
+    hash = "0pwxpry72bv9h0vi0fb8yngz0fiq6ypkrvhzgscmr4q1fjk42p0s";
   })
 ]
